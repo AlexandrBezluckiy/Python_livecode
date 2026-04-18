dad7fbbccf7a3cf1ca940e78b3a71f1b76d872d4
AlexandrBezluckiy <a.bezluckiy@gmail.com>
2026-04-19 01:35:29 +0700

    add 1 task

--- mini_intergration_flow.py
+++ mini_intergration_flow.py
@@ -6,12 +6,51 @@ def send_request(url):
         return {'status': 'error', 'code': response.status_code}
     try:
         response_json = response.json()
-    except:
+        return parse_response(response_json)
+    except ValueError:
         return {'status': 'error', 'code': 'Invalid JSON'}
 
-def parse_response(response):
+def parse_response(response_json):
+    json_data = {}
+    name = response_json.get('name')
+    email = response_json.get('email')
+    address = response_json.get('address', {})
+    city = address.get('city')
+
+    if not name:
+        return {'status': 'error', 'code': 'Name is required'}
+    else :
+        json_data['name'] = name
+    if not email:
+        return {'status': 'error', 'code': 'Email is required'}
+    else :
+        json_data['email'] = email
+    if not address:
+        return {'status': 'error', 'code': 'Address is required'}
+    if not city:
+        return {'status': 'error', 'code': 'City is required'}
+    else:
+        json_data['city'] = city
+    return process_request(json_data)
+
+def process_request(json_data):
+    new_user = json_data.get('name')
+    for item in data_base:
+        if new_user == item.get('name'):
+            return {'status': 'error', 'code': 'Name is already taken'}
+    data_base.append(json_data)
+    send_response()
+    return send_response()
 
 
+def send_response():
+    return {
+        "status": "success",
+        "code": 200,
+        "message": "Application created"
+    }
 
-
+data_base = []
 url = 'https://jsonplaceholder.typicode.com/users/1'
+
+print(send_request(url))
